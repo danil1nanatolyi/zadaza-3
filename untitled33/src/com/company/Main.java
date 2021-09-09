d7953ff220db38aa05ccc33494e0dc35b278a936
danil1nanatolyi <80408487+danil1nanatolyi@users.noreply.github.com>
2021-09-09 23:33:24 +0300

    Add files via upload

---
+++ untitled33/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input grn course: ");
+        double grnCourse = scanner.nextDouble();
+        System.out.print("Input grn count: ");
+        double grnCount = scanner.nextDouble();
+        System.out.println("You can buy "+ convert(grnCourse,grnCount)+" dollars.");
+
+    }
+    static double convert(double grnCourse, double count){
+        return count/grnCourse;
+
+    }
+}
